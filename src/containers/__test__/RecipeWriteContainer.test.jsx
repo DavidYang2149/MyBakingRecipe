250174d181cf9f92a838acf4ad25489f60e2e3d0
davidyang2149 <davidyang2149@gmail.com>
2021-02-14 21:18:29 +0900

    Update RecipeWriteContainer Test
    MyBakingRecipe Mock
    - Update RecipeWriteContainer Test(Coverage 100%).

--- src/containers/__test__/RecipeWriteContainer.test.jsx
+++ src/containers/__test__/RecipeWriteContainer.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import recipes from '../../../fixtures/recipes';
 import allConditionsState from '../../../fixtures/allConditionsState';
@@ -11,23 +11,19 @@ jest.mock('../../services/recipes');
 
 describe('RecipeWriteContainer', () => {
   const dispatch = jest.fn();
-  const handleChange = jest.fn();
 
   beforeEach(() => {
     dispatch.mockClear();
-    handleChange.mockClear();
 
     useDispatch.mockImplementation(() => dispatch);
-    useSelector.mockClear();
+    useSelector.mockImplementation((selector) => selector({
+      ...allConditionsState,
+      recipe: recipes[0],
+    }));
   });
 
-  context('with recipe', () => {
+  describe('with recipe', () => {
     it('renders container', () => {
-      useSelector.mockImplementation((selector) => selector({
-        ...allConditionsState,
-        recipe: recipes[0],
-      }));
-
       render(<RecipeWriteContainer />);
     });
 
@@ -35,16 +31,117 @@ describe('RecipeWriteContainer', () => {
       const { getByLabelText } = render(<RecipeWriteContainer />);
 
       const label = getByLabelText('레시피명');
-      expect(label).toHaveValue('마들렌');
+      expect(label.value).toBe('마들렌');
 
-      // fireEvent.change(label, {
-      //   target: {
-      //     name: 'title',
-      //     value: '파운드케이크',
-      //   },
-      // });
+      fireEvent.change(label, {
+        target: {
+          name: 'title',
+          value: '파운드케이크',
+        },
+      });
 
-      // expect(label).toHaveValue('파운드케이크');
+      expect(dispatch).toBeCalledWith({
+        type: 'recipe/changeRecipe',
+        payload: { name: 'title', value: '파운드케이크' },
+      });
+    });
+
+    it('change ingredient', () => {
+      const { getAllByLabelText } = render(<RecipeWriteContainer />);
+
+      const labels = getAllByLabelText('원재료');
+      const label = labels[1];
+      expect(label.value).toBe('설탕');
+
+      fireEvent.change(label, {
+        target: {
+          name: 'ingredient-1',
+          value: '백설탕',
+        },
+      });
+
+      expect(dispatch).toBeCalledWith({
+        type: 'recipe/changeIngredient',
+        payload: { name: 'ingredient-1', value: '백설탕' },
+      });
+    });
+
+    it('change new ingredient', () => {
+      const { getAllByLabelText } = render(<RecipeWriteContainer />);
+
+      const labels = getAllByLabelText('원재료');
+      const label = labels[0];
+      expect(label.value).toBe('');
+
+      fireEvent.change(label, {
+        target: {
+          name: 'ingredient-5',
+          value: '레몬',
+        },
+      });
+
+      expect(dispatch).toBeCalledWith({
+        type: 'recipe/changeNewIngredient',
+        payload: { name: 'ingredient-5', value: '레몬' },
+      });
+    });
+
+    context('keyup with enter', () => {
+      it('set new ingredient', () => {
+        const { getAllByLabelText } = render(<RecipeWriteContainer />);
+
+        const labels = getAllByLabelText('원재료');
+        const label = labels[0];
+        expect(label.value).toBe('');
+
+        fireEvent.keyUp(label, {
+          key: 'Enter',
+        });
+
+        expect(dispatch).toBeCalledWith({
+          type: 'recipe/setNewIngredient',
+          payload: {
+            fields: {
+              id: 0,
+              ingredient: '',
+              weight: 0,
+            },
+          },
+        });
+      });
+    });
+
+    context('keyup without enter', () => {
+      it('nothing to change', () => {
+        const { getAllByLabelText } = render(<RecipeWriteContainer />);
+
+        const labels = getAllByLabelText('원재료');
+        const label = labels[0];
+        expect(label.value).toBe('');
+
+        fireEvent.keyUp(label, {
+          key: 'Shift',
+        });
+
+        expect(dispatch).toBeCalledTimes(0);
+      });
+    });
+
+    it('click and set new ingredient', () => {
+      const { getByText } = render(<RecipeWriteContainer />);
+
+      fireEvent.click(getByText('추가하기'));
+
+      expect(dispatch).toBeCalledWith({
+        type: 'recipe/setNewIngredient',
+        payload: {
+          fields: {
+            id: 0,
+            ingredient: '',
+            weight: 0,
+          },
+        },
+      });
     });
   });
 });
