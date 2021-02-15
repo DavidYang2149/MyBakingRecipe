b289e82c7416c8f4fc1bacad071d11cb5ddcf9e6
davidyang2149 <davidyang2149@gmail.com>
2021-02-15 10:01:42 +0900

    Update RecipesPage
    MyBakingRecipe Mock
    - Update RecipesPage (divide RecipesContainer).

--- src/pages/RecipesPage.jsx
+++ src/pages/RecipesPage.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import RecipesContainer from '../containers/RecipesContainer';
 
 const RecipesPage = () => {
   return (
-    <ul>
-      <li><Link to="/recipe"><button type="button">마들렌</button></Link></li>
-      <li><Link to="/recipe"><button type="button">까눌레</button></Link></li>
-      <li><Link to="/recipe"><button type="button">크렘 브륄레</button></Link></li>
-    </ul>
+    <RecipesContainer />
   );
 };
 
--- src/pages/__test__/RecipesPage.test.jsx
+++ src/pages/__test__/RecipesPage.test.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { render } from '@testing-library/react';
 
+// import recipes from '../../../fixtures/recipes';
+import allConditionsState from '../../../fixtures/allConditionsState';
 import ReipcesPage from '../RecipesPage';
 
+jest.mock('react-redux');
+jest.mock('../../services/recipes');
+
 describe('ReipcesPage', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      ...allConditionsState,
+      // recipes: recipes,
+    }));
+  });
+
   it('render Page', () => {
     const { container } = render((
       <MemoryRouter>
