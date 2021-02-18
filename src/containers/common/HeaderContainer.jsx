0005bb14aa4d7d1a6624e4dcd36df514288194a0
davidyang2149 <davidyang2149@gmail.com>
2021-02-18 13:59:53 +0900

    Update HeaderContainer
    MyBakingRecipe
    - Update HeaderContainer(inProgress).

--- src/containers/common/HeaderContainer.jsx
+++ src/containers/common/HeaderContainer.jsx
@@ -1,14 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  setUser,
+  clearUser,
+} from '../../redux/user';
+import { auth, provider } from '../../services/firebase';
 
 const HeaderContainer = () => {
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => ({
+    user: state.user,
+  }));
+
+  const {
+    displayName,
+  } = user;
+
+  const signInWithGoogle = async () => {
+    const result = await auth.signInWithPopup(provider);
+    // setUser(result.user.email);
+    // setUser(result.user.displayName);
+    if (result) {
+      dispatch(setUser({ name: 'userId', value: result.user.email }));
+      dispatch(setUser({ name: 'displayName', value: result.user.displayName }));
+    }
+  };
+
+  const signOut = async () => {
+    await auth.signOut();
+    dispatch(clearUser());
+  };
   return (
     <nav>
       <Link to="/">Home</Link>
       <h1>My Baking Recipe</h1>
-      <Link to="/signin">Sign in</Link>
-      <Link to="/signup">Sign up</Link>
-      <button type="button">Logout</button>
+      {
+        displayName
+          ? (
+            <>
+              <p>
+                Welcome,
+                &nbsp;
+                {displayName}
+              </p>
+              <button type="button" onClick={signOut}>Logout</button>
+            </>
+          ) : <button type="button" onClick={signInWithGoogle}>Sign in</button>
+      }
     </nav>
   );
 };
