e3e7ac5a09b6f2a91c1e1b83bbe62739bb300967
davidyang2149 <davidyang2149@gmail.com>
2021-02-18 11:00:49 +0900

    Update Firebase
    MyBakingRecipe
    - Update Firebase (analytics off).

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/app';
 
 import 'firebase/auth';
-import 'firebase/analytics';
+// import 'firebase/analytics';
 import 'firebase/firestore';
 
 import config from '../../config';
@@ -10,8 +10,10 @@ firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 
-export const analytics = firebase.analytics();
+// export const analytics = firebase.analytics();
 
 export const fireStore = firebase.firestore;
 
 export const db = firebase.firestore();
+
+export const provider = new firebase.auth.GoogleAuthProvider();
