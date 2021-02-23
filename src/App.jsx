ac685d421cc5240a72ee955184f5c82ab537e336
davidyang2149 <davidyang2149@gmail.com>
2021-02-23 20:57:36 +0900

    Update App (Add recipewrite)
    MyBakingRecipe
    - Update App (Add Route recipewrite).

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import HeaderContainer from './containers/common/HeaderContainer';
 import RecipesPage from './pages/RecipesPage';
 import RecipePage from './pages/RecipePage';
+import RecipeWritePage from './pages/RecipeWritePage';
 import Recipe404Page from './pages/Recipe404Page';
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={RecipesPage} />
         <Route path="/recipe/:id" component={RecipePage} />
+        <Route path="/recipewrite/:id" component={RecipeWritePage} />
         <Route component={Recipe404Page} />
       </Switch>
     </>
