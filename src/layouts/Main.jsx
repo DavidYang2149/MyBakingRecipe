ff0cec8c1eec3c1783f494c5ee09adbe0d66e143
davidyang2149 <davidyang2149@gmail.com>
2021-03-07 13:26:52 +0900

    Update Main (CSS)
    MyBakingRecipe
    - Update Main (CSS).

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -4,12 +4,13 @@ const Main = styled.div`
   box-sizing: border-box;
   background-color: #fff;
   padding-top: 30px;
+  padding-bottom: 30px;
   /* text-align: center; */
   margin: 20px auto;
   height: 100vh;
   
   @media screen and (min-width: 768px) {
-    width: 700px;
+    width: 600px;
   }
 `;
 
