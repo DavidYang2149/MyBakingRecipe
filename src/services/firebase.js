8f1846236b306b5ec6d45ac3b99ac9e01e65943e
davidyang2149 <davidyang2149@gmail.com>
2021-03-22 10:54:57 +0900

    Update firebase services (Add storage) #7
    MyBakingRecipe
    - Update firebase services (Add storage).

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,6 +4,8 @@ import 'firebase/auth';
 // import 'firebase/analytics';
 import 'firebase/firestore';
 
+import 'firebase/storage';
+
 import config from '../../config';
 
 firebase.initializeApp(config);
@@ -19,3 +21,5 @@ export const fireStore = firebase.firestore;
 export const db = firebase.firestore();
 
 export const provider = new firebase.auth.GoogleAuthProvider();
+
+export const storage = firebase.storage();
