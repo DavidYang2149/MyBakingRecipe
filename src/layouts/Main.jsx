1b367f174ef41e9d51c3811ff11dc1839eea5cad
davidyang2149 <davidyang2149@gmail.com>
2021-03-27 20:54:05 +0900

    Update Main layouts (CSS)
    MyBakingRecipe
    - Update Main layouts (CSS).

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -5,13 +5,21 @@ const Main = styled.div`
   background-color: #fff;
   padding-top: 30px;
   padding-bottom: 30px;
-  /* text-align: center; */
   margin: 20px auto;
+  width: 100vw;
   height: 100vh;
   
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: 600px) {
     width: 600px;
   }
+
+  @media screen and (min-width: 768px) {
+    width: 768px;
+  }
+
+  /* @media screen and (min-width: 1024px) {
+    width: 1024px;
+  } */
 `;
 
 export default Main;
