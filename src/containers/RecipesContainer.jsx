8dd8b7aecd4cbe8b206a9cea80ac5cc5147aeb7c
davidyang2149 <davidyang2149@gmail.com>
2021-03-27 20:51:26 +0900

    Update RecipesContainer (Add Loading)
    MyBakingRecipe
    - Update RecipesContainer (Add Loading).

--- src/containers/RecipesContainer.jsx
+++ src/containers/RecipesContainer.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import RecipeList from '../components/RecipeList';
+import UseLoading from '../components/UseLoading';
+import { isEmpty } from '../utils/utils';
 
 const RecipesContainer = () => {
   const { recipes: { recipesBook } } = useSelector((state) => ({
     recipes: state.recipes,
   }));
+
+  if (isEmpty(recipesBook.length)) {
+    return (
+      <UseLoading />
+    );
+  }
 
   return (
     <article>
