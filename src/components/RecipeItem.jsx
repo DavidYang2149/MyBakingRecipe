3beda750f1d24e05db16043734694ea296bec15a
davidyang2149 <davidyang2149@gmail.com>
2021-03-27 20:56:45 +0900

    Update RecipeItem (html)
    MyBakingRecipe
    - Update RecipeItem (html).
    - for CSS.

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -10,16 +10,20 @@ const RecipeItem = ({ recipe }) => {
   }
 
   return (
-    <RecipeCard className="card-item">
+    <RecipeCard>
       <Link to={`/recipe/${recipe.id}`}>
-        <figure className="card-image">
+        <figure className="card-image" style={{ backgroundImage: `url(${recipe.image || 'images/cookie.jpeg'})` }}>
           <img src={recipe.image || 'images/cookie.jpeg'} alt="recipe" />
         </figure>
         <div className="card-desc">
-          {recipe.title}
-        </div>
-        <div className="card-desc">
-          {recipe.created.substr(0, 10)}
+          <h1>{recipe.title}</h1>
+          <p>{recipe.description.length > 30 ? `${recipe.description.substring(0, 30)}...` : recipe.description}</p>
+          <p className="card-created">{recipe.created.substr(0, 10)}</p>
+          <p>
+            by
+            {' '}
+            <strong>{recipe.userId.split('@')[0]}</strong>
+          </p>
         </div>
       </Link>
     </RecipeCard>
