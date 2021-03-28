d74f6ce88f11692ca8e21b48123b663484e0ab11
davidyang2149 <davidyang2149@gmail.com>
2021-03-28 12:19:35 +0900

    Update Header layouts (CSS)
    MyBakingRecipe
    - Update Header layouts (CSS).
    - Update HeaderButtons CSS.

--- src/layouts/common/Header.jsx
+++ src/layouts/common/Header.jsx
@@ -5,9 +5,7 @@ display: flex;
 justify-content: space-between;
 background: #fff;
 align-items: center;
-/* opacity: 0.5; */
 padding: 10px;
-/* border: 1px solid #b1a0a0; */
 `;
 
 const HeaderTitle = styled.h1`
@@ -15,6 +13,7 @@ const HeaderTitle = styled.h1`
   font-size: 2rem;
   font-weight: 800;
   font-family: sans-serif;
+  padding-left: 20px;
   
   @media screen and (max-width: 768px) {
     display: none;
@@ -22,18 +21,31 @@ const HeaderTitle = styled.h1`
 `;
 
 const HeaderUser = styled.p`
+  font-size: 1.1rem;
   font-weight: 700;
+  margin: 10px;
 `;
 
 const HeaderButton = styled.button`
   color: #361d74;
+  margin: 10px;
   padding: 5px 7px;
   font-size: 1.025rem;
   font-weight: 600;
-  background: transparent;
   border-radius: 6px;
   border-color: #361d74;
+  background: transparent;
   cursor: pointer;
+
+  @media screen and (max-width: 600px) {
+    display: none;
+  }
+`;
+
+const ButtonLines = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export {
@@ -41,4 +53,5 @@ export {
   HeaderTitle,
   HeaderUser,
   HeaderButton,
+  ButtonLines,
 };
