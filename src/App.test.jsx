d36fe9dce2d6451e5a8e96eb7d0ec2f71c6e0b5d
davidyang2149 <davidyang2149@gmail.com>
2021-04-03 18:00:52 +0900

    Update App With TestCode (React lazy, suspense)
    MyBakingRecipe
    - Update App With TestCode.
      - React Code split(lazy, suspense).

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 
 import App from './App';
 import allConditionsState from '../fixtures/allConditionsState';
@@ -33,16 +33,32 @@ describe('App', () => {
   };
 
   context('with path /', () => {
-    it('renders RecipesPage', () => {
-      const { container } = renderApp({ path: '/' });
+    it('renders RecipesPage', async () => {
+      const { container } = await waitFor(() => renderApp({ path: '/' }));
 
       expect(container).toHaveTextContent('마들렌');
     });
   });
 
+  context('with path /recipe/:id', () => {
+    it('renders RecipesPage', async () => {
+      const { container } = await waitFor(() => renderApp({ path: '/recipe/1' }));
+
+      expect(container).toHaveTextContent('마들렌');
+    });
+  });
+
+  context('with path /recipewrite/:id', () => {
+    it('renders RecipesPage', async () => {
+      const { container } = await waitFor(() => renderApp({ path: '/recipewrite/1' }));
+
+      expect(container).toHaveTextContent('해당 페이지를 찾을 수 없습니다.');
+    });
+  });
+
   context('with path /notExist', () => {
-    it('renders Recipe404Page', () => {
-      const { container } = renderApp({ path: '/notExist' });
+    it('renders Recipe404Page', async () => {
+      const { container } = await waitFor(() => renderApp({ path: '/notExist' }));
 
       expect(container).toHaveTextContent('해당 페이지를 찾을 수 없습니다.');
     });
